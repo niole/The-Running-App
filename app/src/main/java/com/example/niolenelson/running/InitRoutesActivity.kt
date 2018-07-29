0f65a8d41d438945070af65843f9348fd34f22f8
niole <niolenelson@gmail.com>
2018-07-29 15:41:59 -0700

    on submit see spinner in first activity, reset spinner on activity resume

--- app/src/main/java/com/example/niolenelson/running/InitRoutesActivity.kt
+++ app/src/main/java/com/example/niolenelson/running/InitRoutesActivity.kt
@@ -3,9 +3,12 @@ package com.example.niolenelson.running
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.ProgressBar
 
 /**
  * Created by niolenelson on 7/29/18.
@@ -17,11 +20,24 @@ class InitRoutesActivity : AppCompatActivity() {
         setButton()
     }
 
+    override fun onResume() {
+        super.onResume()
+        val button = findViewById<Button>(R.id.route_length_input_submit)
+        val spinner = findViewById<ProgressBar>(R.id.loadingPanel)
+        button.visibility = View.VISIBLE
+        findViewById<LinearLayout>(R.id.init_routes_container ).removeView(spinner)
+    }
+
+
     private fun setButton() {
         val button = findViewById<Button>(R.id.route_length_input_submit)
         button.setOnClickListener {
             view: View ->
             // TODO validate input
+            button.visibility = View.GONE
+            val inflater = LayoutInflater.from(this)
+            val spinner = inflater.inflate(R.layout.spinner, null, false)
+            findViewById<LinearLayout>(R.id.init_routes_container ).addView(spinner)
             val text: String = findViewById<EditText>(R.id.route_length_input).text.toString()
             val routeDistanceMeters = text.toInt()
             val intent = Intent(this, MapsActivity::class.java)
--- app/src/main/java/com/example/niolenelson/running/MapsActivity.kt
+++ app/src/main/java/com/example/niolenelson/running/MapsActivity.kt
@@ -4,7 +4,10 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.ProgressBar
 import com.example.niolenelson.running.utilities.AngleGetter
 import com.example.niolenelson.running.utilities.Haversine
 import com.google.android.gms.maps.CameraUpdateFactory
