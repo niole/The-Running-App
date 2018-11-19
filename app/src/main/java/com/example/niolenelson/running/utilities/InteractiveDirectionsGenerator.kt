da166ff41471fdc7082b19d61127402305f782ad
Niole Nelson <niolenelson@gmail.com>
2018-11-18 20:46:29 -0800

    Create relatively good forward direction only route generator

--- app/src/main/java/com/example/niolenelson/running/utilities/InteractiveDirectionsGenerator.kt
+++ app/src/main/java/com/example/niolenelson/running/utilities/InteractiveDirectionsGenerator.kt
@@ -1,26 +1,23 @@
 package com.example.niolenelson.running.utilities
 
 import android.app.Activity
+import android.widget.Toast
+import com.google.android.gms.maps.model.LatLng as BaseLatLng
 import com.google.android.gms.maps.GoogleMap
 import com.google.maps.model.LatLng
 
 /**
  * When the user is on a segment of a path, that path is highlighted on the map
  */
-class InteractiveDirectionsGenerator(val activity: Activity, mMap: GoogleMap, val directions: List<LocalDirectionApi.Direction>) {
+class InteractiveDirectionsGenerator(val activity: Activity, val mMap: GoogleMap, val directions: List<LocalDirectionApi.Direction>) {
+
+    private val minDiff: Double = 0.05
 
     private val locationUpdater: LocationUpdater = LocationUpdater(activity, mMap)
 
     private val directionChunks: List<List<LatLng>> = directions.map { it.polyline.decodePath() }
 
-    /**
-     * communicates what direction in the route the user travels in
-     * if 1, the user travels in the indexes positively
-     * if -1, the user travels through the directions negatively
-     * assumed to be positive to start
-     * if the user decides to go the opposite way in the route, it will change
-     */
-    private var travelDirection: Int = 1
+    private var announcedForChunks: MutableList<Boolean> = MutableList(directionChunks.size) { false }
 
     /**
      * the index of the chunk direction where it was last determined that the
@@ -42,10 +39,10 @@ class InteractiveDirectionsGenerator(val activity: Activity, mMap: GoogleMap, va
     private var endChunkIndex: Int = -1
 
     init {
+        //test()
         locationUpdater.onLocationUpdate {
             lat, lng ->
-            println("Latitude $lat")
-            println("Longitude $lng")
+            getDirectionsFromLocation(lat, lng)
         }
     }
 
@@ -55,13 +52,97 @@ class InteractiveDirectionsGenerator(val activity: Activity, mMap: GoogleMap, va
         // what if a person gets off of the route but then gets back on the route?
         // lets deal with that later
         // if we're within a 5 meters of the current chunk perpendicularly, still in chunk
-        // if within .12 miles of next chunk, announce
-        val stillInChunk = true
-        if (stillInChunk) {
-            // check to see how far we are to next or previous chunk
-        } else {
-            // find out what chunk index we're at
+        // if within tome diff miles of next chunk, announce
+        likelyChunkIndex = getCurrentChunkIndex(lat, lng)
+        if (likelyChunkIndex > -1) {
+            doAnnounceDirections(lat, lng)
+            if (isRouteOver(lat, lng)) {
+                println("ROUTE OVER YAY")
+                Toast.makeText(activity, "You did it!", Toast.LENGTH_LONG)
+            }
         }
+    }
+
+    private fun doAnnounceDirections(lat: Double, lng: Double) {
+        val currentChunk = directionChunks[likelyChunkIndex]
+        if (shouldDoAnnouncement(currentChunk.last(), lat, lng)) {
+            // going forwards
+            announcedForChunks[likelyChunkIndex] = true
+            doFirstAnnouncement(likelyChunkIndex)
+            likelyChunkIndex = Math.min(likelyChunkIndex + 1, directionChunks.size - 1)
+        }
+    }
+
+    private fun isRouteOver(lat: Double, lng: Double): Boolean {
+        val currentChunk = directionChunks[likelyChunkIndex]
+        return shouldDoAnnouncement(currentChunk.last(), lat, lng)
+    }
+
+    private fun getCurrentChunkIndex(lat: Double, lng: Double): Int {
+        if (likelyChunkIndex == -1) {
+            // at start
+            // determine current chunk, likely direction, start and end chunk
+            // assume at chunk 0 at first and if not, search everywhere
+            likelyChunkIndex = 0
+            startChunkIndex = 0
+            endChunkIndex = directionChunks.size - 1
+        }
+
+        val likelyStartChunk = directionChunks[likelyChunkIndex]
+        if (!locationInChunk(lat, lng, likelyStartChunk)) {
+
+            // pick from one chunk previous just in case we moved forwards too soon
+            val startIndex = Math.max(likelyChunkIndex - 1, 0)
+            for (i in (startIndex until directionChunks.size)) {
+                val chunk = directionChunks[i]
+                if (locationInChunk(lat, lng, chunk)) {
+                    // hooray
+                    return i
+                }
+            }
+        }
+        return likelyChunkIndex
+    }
+
+    /**
+     * determines whether lat lng pair is within .1 miles of chunk
+     */
+    private fun locationInChunk(lat: Double, lng: Double, chunk: List<LatLng>): Boolean {
+        if (chunk.isNotEmpty()) {
+            var point = chunk.first()
+            if (chunk.size > 1) {
+                point = chunk.reduce {
+                    currentClosestPoint, nextPoint ->
+                    val distanceToCurrent = getDistanceBetweenPoints(currentClosestPoint, lat, lng)
+                    val distanceToNext = getDistanceBetweenPoints(nextPoint, lat, lng)
+                    if (distanceToNext < distanceToCurrent) {
+                        nextPoint
+                    } else {
+                        currentClosestPoint
+                    }
+                }
+            }
+            // TODO this could be better
+            // currently just does distance to closest point
+            // should do distance from route along perpendicular line
+            val distance = getDistanceBetweenPoints(point, lat, lng)
+            return distance <= minDiff
+        }
+        return false
+    }
+
+    private fun getDistanceBetweenPoints(basePoint: LatLng, lat: Double, lng: Double): Double {
+        return Haversine.distanceMiles(lat, lng, basePoint.lat, basePoint.lng)
+    }
+
+    private fun shouldDoAnnouncement(endpoint: LatLng, lat: Double, lng: Double): Boolean {
+        val nextDiff = getDistanceBetweenPoints(endpoint, lat, lng)
+        val shouldAnnounce = nextDiff <= minDiff
+        return shouldAnnounce && !announcedForChunks[likelyChunkIndex]
+    }
+
+    private fun doFirstAnnouncement(chunkIndex: Int) {
+        println("ANNOUNCING $chunkIndex, ${directions[chunkIndex].htmlInstructions}")
     }
 
     fun stopLocationUpdates() {
@@ -76,4 +157,16 @@ class InteractiveDirectionsGenerator(val activity: Activity, mMap: GoogleMap, va
         return locationUpdater.isRequestingLocationUpdates()
     }
 
+    private fun test() {
+        val points = directionChunks.flatten()
+        var i = 0
+        while (i < points.size) {
+            val it = points[i]
+            activity.runOnUiThread {
+                getDirectionsFromLocation(it.lat, it.lng)
+            }
+            i += 1
+        }
+        println(directions.joinToString { "  |  ${it.htmlInstructions}" })
+    }
 }
