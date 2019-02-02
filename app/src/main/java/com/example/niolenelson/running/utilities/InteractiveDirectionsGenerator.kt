0c5029b95bfbc6a02c63ba5d02d110383fcfcb93
Niole Nelson <niolenelson@gmail.com>
2019-02-02 11:38:47 -0800

    Improve route generation

--- app/src/main/java/com/example/niolenelson/running/utilities/InteractiveDirectionsGenerator.kt
+++ app/src/main/java/com/example/niolenelson/running/utilities/InteractiveDirectionsGenerator.kt
@@ -3,35 +3,54 @@ package com.example.niolenelson.running.utilities
 import com.google.android.gms.maps.model.LatLng as BaseLatLng
 import com.google.maps.model.LatLng
 
+/**
+ * This takes the location of the user and announces a direction if necessary
+ *
+ * Don't want to announce the same directions twice
+ * Want to announce the directions in order
+ * Want to announce the directions when it makes sense
+ *
+ * This is done by
+ * knowing what announcements have already happened
+ * knowing what segment the user is closest to
+ *
+ * should take into account how large the current segment is so can find tune
+ * Maybe should always pick a point to be within a certain distance of in order to
+ * announce the next direction
+ *
+ * It's all about adapting it to what the location updater will do
+ */
+
 data class Instruction(
         val points: List<LatLng>,
         val humanDirections: String
 )
 
-open class InteractiveDirectionsGenerator(val locationUpdater: LocationUpdaterBase, val handleAnnoucement: (msg: String) -> Int, val stopAnnoucenemtns: () -> Unit, val onRouteEnd: () -> Unit, val directions: List<Instruction>) {
+data class CurrentLocation(
+        val directionIndex: Int,
+        val closestLatLngIndex: Int,
+        val currentLatLng: LatLng,
+        val instruction: Instruction
+) {
+    fun closeEnoughToAnnounce(minDiffMiles: Double): Boolean {
+        val endPoint = instruction.points.last()
+        return Haversine.distanceMiles(currentLatLng.lat, currentLatLng.lng, endPoint.lat, endPoint.lng) <= minDiffMiles
+    }
+}
 
-    private val minDiff: Double = 0.2
+open class InteractiveDirectionsGenerator(
+        val locationUpdater: LocationUpdaterBase,
+        val handleAnnoucement: (msg: String) -> Int,
+        val stopAnnoucenemtns: () -> Unit,
+        val onRouteEnd: () -> Unit,
+        val directions: List<Instruction>
+    ) {
+
+    private var currentLocation: CurrentLocation = CurrentLocation(0, 0, directions[0].points[0], directions[0])
+
+    private val minDiffMiles: Double = 0.05
 
     private var announcedForChunks: MutableList<Boolean> = MutableList(directions.size) { false }
-
-    /**
-     * the index of the chunk direction where it was last determined that the
-     * user is at
-     * initialized once location starts to be reported
-     */
-    private var likelyChunkIndex: Int = -1
-
-    /**
-     * the index of the chunk where the user started their route
-     * initialized after the user starts the run, will mostly likely be chunk 0
-     */
-    private var startChunkIndex: Int = -1
-
-    /**
-     * the index of the chunk which is the last chunk in the user's route
-     * initialized after start chunk is determined
-     */
-    private var endChunkIndex: Int = -1
 
     init {
         locationUpdater.onLocationUpdate { lat, lng ->
@@ -42,101 +61,13 @@ open class InteractiveDirectionsGenerator(val locationUpdater: LocationUpdaterBa
 
 
     fun getDirectionsFromLocation(lat: Double, lng: Double) {
-        // if two parts of the route are really close together, we can't have any
-        // hopping between parts of the routes
-        // what if a person gets off of the route but then gets back on the route?
-        // lets deal with that later
-        // if we're within a 5 meters of the current chunk perpendicularly, still in chunk
-        // if within tome diff miles of next chunk, announce
-        likelyChunkIndex = getCurrentChunkIndex(lat, lng)
-        if (likelyChunkIndex > -1) {
-            doAnnounceDirections(lat, lng)
-            if (isRouteOver(lat, lng)) {
-                onRouteEnd()
-            }
+        // need a current chunk index for expediency and future inference
+        // also need the current location what we are closest to in the current chunk
+        currentLocation = getNextLocation(lat, lng, currentLocation)
+        doAnnounceDirections(currentLocation)
+        if (isRouteOver()) {
+            onRouteEnd()
         }
-    }
-
-    private fun doAnnounceDirections(lat: Double, lng: Double) {
-        val currentChunk = directions[likelyChunkIndex]
-        if ((likelyChunkIndex == 0 && !announcedForChunks[0]) || shouldDoAnnouncement(currentChunk.points.last(), lat, lng)) {
-            // going forwards
-            announcedForChunks[likelyChunkIndex] = true
-            doFirstAnnouncement(likelyChunkIndex)
-            likelyChunkIndex = Math.min(likelyChunkIndex + 1, directions.size - 1)
-        }
-    }
-
-    private fun isRouteOver(lat: Double, lng: Double): Boolean {
-        val currentChunk = directions[likelyChunkIndex]
-        return shouldDoAnnouncement(currentChunk.points.last(), lat, lng)
-    }
-
-    private fun getCurrentChunkIndex(lat: Double, lng: Double): Int {
-        if (likelyChunkIndex == -1) {
-            // at start
-            // determine current chunk, likely direction, start and end chunk
-            // assume at chunk 0 at first and if not, search everywhere
-            likelyChunkIndex = 0
-            startChunkIndex = 0
-            endChunkIndex = directions.size - 1
-        }
-
-        val likelyStartChunk = directions[likelyChunkIndex].points
-        if (!locationInChunk(lat, lng, likelyStartChunk)) {
-
-            // pick from one chunk previous just in case we moved forwards too soon
-            val startIndex = Math.max(likelyChunkIndex - 1, 0)
-            for (i in (startIndex until directions.size)) {
-                val chunk = directions[i]
-                if (locationInChunk(lat, lng, chunk.points)) {
-                    // hooray
-                    return i
-                }
-            }
-        }
-        return likelyChunkIndex
-    }
-
-    /**
-     * determines whether lat lng pair is within .1 miles of chunk
-     */
-    private fun locationInChunk(lat: Double, lng: Double, chunk: List<LatLng>): Boolean {
-        if (chunk.isNotEmpty()) {
-            var point = chunk.first()
-            if (chunk.size > 1) {
-                point = chunk.reduce {
-                    currentClosestPoint, nextPoint ->
-                    val distanceToCurrent = getDistanceBetweenPoints(currentClosestPoint, lat, lng)
-                    val distanceToNext = getDistanceBetweenPoints(nextPoint, lat, lng)
-                    if (distanceToNext < distanceToCurrent) {
-                        nextPoint
-                    } else {
-                        currentClosestPoint
-                    }
-                }
-            }
-            // TODO this could be better
-            // currently just does distance to closest point
-            // should do distance from route along perpendicular line
-            val distance = getDistanceBetweenPoints(point, lat, lng)
-            return distance <= minDiff
-        }
-        return false
-    }
-
-    private fun getDistanceBetweenPoints(basePoint: LatLng, lat: Double, lng: Double): Double {
-        return Haversine.distanceMiles(lat, lng, basePoint.lat, basePoint.lng)
-    }
-
-    private fun shouldDoAnnouncement(endpoint: LatLng, lat: Double, lng: Double): Boolean {
-        val nextDiff = getDistanceBetweenPoints(endpoint, lat, lng)
-        val shouldAnnounce = nextDiff <= minDiff
-        return shouldAnnounce && !announcedForChunks[likelyChunkIndex]
-    }
-
-    private fun doFirstAnnouncement(chunkIndex: Int) {
-        handleAnnoucement(directions[chunkIndex].humanDirections)
     }
 
     fun stopLocationUpdates() {
@@ -150,5 +81,79 @@ open class InteractiveDirectionsGenerator(val locationUpdater: LocationUpdaterBa
 
     fun isRequestingLocationUpdates(): Boolean {
         return locationUpdater.isRequestingLocationUpdates()
+    }
+
+    /**
+     * Always announce right at start
+     */
+    private fun doAnnounceDirections(currentLocation: CurrentLocation) {
+        val shouldAnnounceForStart = !announcedForChunks[0] && currentLocation.directionIndex == 0
+        if (shouldAnnounceForStart || (currentLocation.closeEnoughToAnnounce(minDiffMiles) && !announcedForChunks[currentLocation.directionIndex])) {
+            announcedForChunks[currentLocation.directionIndex] = true
+            handleAnnoucement(currentLocation.instruction.humanDirections)
+        }
+    }
+
+    /**
+     * Route is over if should do announcement for last chunk
+     */
+    private fun isRouteOver(): Boolean {
+        return announcedForChunks[directions.size - 1]
+    }
+
+    private fun getNextLocation(currentLat: Double, currentLng: Double, previousLocation: CurrentLocation): CurrentLocation {
+        val likelyCurrentStep = directions[previousLocation.directionIndex].points
+        val likelyCurrentLatLng = likelyCurrentStep[previousLocation.closestLatLngIndex]
+        val distanceToPreviousLocation = getMilesDistanceBetweenPoints(likelyCurrentLatLng, currentLat, currentLng)
+
+        // check in whatever direction you want and if distance grows, user didn't go that way
+        // if there is no closer point than current, then stay with current
+        // check if they went forwards because directions only work if you're going forwards
+        // zero in on closest location, but within reason
+
+        // attempt forward search
+        var closestDistance = distanceToPreviousLocation
+        var closestLocation = previousLocation
+        for (stepIndex in previousLocation.directionIndex..(directions.size - 1)) {
+            var startLocationIndex = 0
+            if (stepIndex == previousLocation.directionIndex) {
+                startLocationIndex = previousLocation.closestLatLngIndex
+            }
+
+            val currentStepPoints = directions[stepIndex].points
+            if (startLocationIndex < currentStepPoints.size - 1) { // if false, compare to next step's first index, TODO potentially you could always just miss an announcment completely
+                for (i in (startLocationIndex + 1)..(currentStepPoints.size - 1)) {
+                    if (isCloserLocation(currentLat, currentLng, currentStepPoints[i], closestLocation.currentLatLng, closestDistance)) {
+                        closestLocation = CurrentLocation(stepIndex, i, currentStepPoints[i], directions[stepIndex])
+                        closestDistance = getMilesDistanceBetweenPoints(currentStepPoints[i], currentLat, currentLng)
+                    } else {
+                        // TODO returning as soon as something is slightly longer than a previous reading is not perfect
+                        // what if the user is on a windy path and their location isn't updating that often
+                        // this current approach only works if the location updates often and the path isn't that windy
+                        // probably should search more places but then infer that the user is in a certain place based on the assumption
+                        // that the user goes forwards
+                        return closestLocation
+                    }
+                }
+            }
+        }
+
+        return closestLocation
+    }
+
+    private fun isCloserLocation(currentLat: Double, currentLng: Double, currentlyExaminedLocation: LatLng, currentlyDeterminedClosesLocation: LatLng, distanceToPreviousLocation: Double): Boolean {
+        val distanceToCurrentLocation = getMilesDistanceBetweenPoints(currentlyExaminedLocation, currentLat, currentLng)
+        val distanceToPreviousPossibleNewLocation = getMilesDistanceBetweenPoints(currentlyDeterminedClosesLocation, currentLat, currentLng)
+
+        // only update index if distance less than previous location
+        if (distanceToCurrentLocation < distanceToPreviousLocation) {
+            // only keep going if distance to current location is less than the previous possible new location
+            return distanceToPreviousPossibleNewLocation > distanceToCurrentLocation
+        }
+        return false
+    }
+
+    private fun getMilesDistanceBetweenPoints(basePoint: LatLng, lat: Double, lng: Double): Double {
+        return Haversine.distanceMiles(lat, lng, basePoint.lat, basePoint.lng)
     }
 }
--- app/src/test/java/com/example/niolenelson/running/InteractiveDirectionsGeneratorTest.kt
+++ app/src/test/java/com/example/niolenelson/running/InteractiveDirectionsGeneratorTest.kt
@@ -5,32 +5,89 @@ import com.example.niolenelson.running.utilities.Instruction
 import com.example.niolenelson.running.utilities.InteractiveDirectionsGenerator
 import com.example.niolenelson.running.utilities.LocationUpdaterBase
 import com.google.maps.model.LatLng
-import junit.framework.Assert.assertEquals
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+
+
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class InteractiveDikrectionsGeneratorTest {
     @Test
-    fun shouldReturnFirstInstructionIfRightOnLocation() {
+    fun should_always_announce_first_instruction_immediately() {
+        val steps = MockData.getDecodedPolyline(MockData.stepsWithOverlap)
+
+        var actual = ""
         val handleAnnouncement = { msg: String ->
-            assertEquals("Announcement should be for first route instruction", "0.0", msg)
+            actual = msg
             1
         }
-        val locationUpdater = setupAssertion(handleAnnouncement,
-                List(5, { index: Int -> index.toDouble() }).map { it: Double ->  Instruction(listOf(LatLng(it, it)),  it.toString()) })
-        locationUpdater.locationUpdateCallback(0.0, 0.0)
+
+        val locationUpdater = setupAssertion(handleAnnouncement, createInstructions(steps))
+
+        val firstStep = steps.first()
+        val firstInstruction = firstStep.first()
+
+        locationUpdater.locationUpdateCallback(firstInstruction.lat, firstInstruction.lng)
+        assertEquals("0", actual, "Should announce if right at last location")
     }
 
     @Test
-    fun shouldReturnThirdInstructionIfRightOnLocation() {
+    fun should_not_announce_first_instruction_if_at_start_of_longer_route_and_not_close_to_start() {
+        val expected = "no announcement"
+        val steps = MockData.getDecodedPolyline(MockData.stepsWithOverlap)
+
+        var actual = expected
         val handleAnnouncement = { msg: String ->
-            assertEquals("Announcement should be for third route instruction", "2.0", msg)
+            actual = msg
             1
         }
-        val locationUpdater = setupAssertion(handleAnnouncement,
-                List(5, { index: Int -> index.toDouble() }).map { it: Double -> Instruction(listOf(LatLng(it, it)), it.toString()) })
-        locationUpdater.locationUpdateCallback(2.0, 2.0)
+        val locationUpdater = setupAssertion(handleAnnouncement, createInstructions(steps))
+        val thirdStep = steps[1]
+        val firstInstruction = thirdStep[2]
+        locationUpdater.locationUpdateCallback(firstInstruction.lat, firstInstruction.lng)
+
+        assertEquals(expected, actual, "Should not announce if at beginning of step and step is long")
+    }
+
+    @Test
+    fun should_announce_first_instruction_if_nearing_end_of_route() {
+        val expected = "1"
+        val steps = MockData.getDecodedPolyline(MockData.stepsWithOverlap)
+
+        var actual = ""
+        val handleAnnouncement = { msg: String ->
+            actual = msg
+            1
+        }
+        val locationUpdater = setupAssertion(handleAnnouncement, createInstructions(steps))
+        val step = steps[1]
+        val firstInstruction = step[7]
+        locationUpdater.locationUpdateCallback(firstInstruction.lat, firstInstruction.lng)
+
+        assertEquals(expected, actual, "Should announce if at nearing end of step")
+    }
+
+    @Test
+    fun should_not_announce_21_instruction_if_in_middle_of_longer_route() {
+        val expected = "no announcement"
+        val steps = MockData.getDecodedPolyline(MockData.stepsWithOverlap)
+
+        var actual = expected
+        val handleAnnouncement = { msg: String ->
+            println("message: $msg")
+            actual = msg
+            1
+        }
+        val locationUpdater = setupAssertion(handleAnnouncement, createInstructions(steps))
+        val step = steps[21]
+        val firstInstruction = step[4]
+        for (i in 0..(steps.size - 1)) {
+            println(i)
+            locationUpdater.locationUpdateCallback(steps[i].last().lat, steps[i].last().lng)
+        }
+
+        assertEquals(expected, actual, "Should not announce if at beginning of step and step is long")
     }
 
     @Test
@@ -97,23 +154,47 @@ class InteractiveDikrectionsGeneratorTest {
 
     @Test
     fun shouldNotAnnounceSameDirectionTwice() {
+        val steps = MockData.getDecodedPolyline(MockData.stepsWithOverlap)
         var count = 0
         val handleAnnouncement = { _: String ->
             count += 1
-            assertEquals("Announcement should only happen once", 1, count)
+            assertEquals( 1, count)
             1
         }
-        val locationUpdater = setupAssertion(handleAnnouncement,
-                List(5, { index: Int -> index.toDouble() }).map { it: Double ->
-                    val base = it * 5
-                    val subSteps = List(5, { it.toDouble() }).map {LatLng(base + it, base + it)}
-                    Instruction(subSteps, base.toString())
-                })
-        val x = Haversine.getLocationXDistanceFromLocationKM(10.0, 10.0, .16, 90.0)
+        val locationUpdater = setupAssertion(handleAnnouncement, createInstructions(steps))
+
+        val instruction = steps[0]
+        val location = instruction[0]
+
+        val x = Haversine.getLocationXDistanceFromLocationKM(location.lat, location.lng, .16, 90.0)
 
         locationUpdater.locationUpdateCallback(x.lat, x.lng)
         locationUpdater.locationUpdateCallback(x.lat, x.lng)
     }
+
+    @Test
+    fun should_not_read_return_instructions_before_beginning_ones_even_if_location_is_the_same() {
+        val steps = MockData.getDecodedPolyline(MockData.stepsWithOverlap)
+        val handleAnnouncement = { announcement: String ->
+            println(announcement)
+//            assertEquals("Announcement should be 21","21", announcement)
+            1
+        }
+        val locationUpdater = setupAssertion(handleAnnouncement, createInstructions(steps))
+
+        val initialInstruction = steps[22]
+        val location = initialInstruction.last()
+        locationUpdater.locationUpdateCallback(location.lat, location.lng)
+        locationUpdater.locationUpdateCallback(location.lat, location.lng)
+        locationUpdater.locationUpdateCallback(location.lat, location.lng)
+        locationUpdater.locationUpdateCallback(location.lat, location.lng)
+        locationUpdater.locationUpdateCallback(location.lat, location.lng)
+        locationUpdater.locationUpdateCallback(location.lat, location.lng)
+        locationUpdater.locationUpdateCallback(location.lat, location.lng)
+        locationUpdater.locationUpdateCallback(location.lat, location.lng)
+        locationUpdater.locationUpdateCallback(location.lat, location.lng)
+    }
+
 
     fun setupAssertion(handleAnnouncement: (msg: String) -> Int, instructions: List<Instruction>): LocationUpdaterBase {
         class L : LocationUpdaterBase()
@@ -129,5 +210,14 @@ class InteractiveDikrectionsGeneratorTest {
 
         locationUpdater.startLocationUpdates()
         return locationUpdater
+    }
+
+    private fun getInstructionsWithOverlaps(): List<Instruction> {
+        val steps = MockData.getDecodedPolyline(MockData.stepsWithOverlap)
+        return createInstructions(steps)
+    }
+
+    private fun createInstructions(steps: List<List<LatLng>>): List<Instruction> {
+        return steps.mapIndexed { index, steps -> Instruction(steps, index.toString()) }
     }
 }
